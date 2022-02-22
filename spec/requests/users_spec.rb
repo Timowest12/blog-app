ac9966bbd1510d27276902f613f3f123a53774ae
Timo <timowester@gmail.com>
2022-02-22 23:04:16 +0100

    Add users index tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
+    before(:each) { get users_path }
+
+    it 'index body returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'index page includes correct placeholder text' do
+      expect(response.body).to include('this is the main users page')
+    end
   end
+
 end
