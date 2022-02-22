ec8dd95b0e4b58f5e1a4903b28a816c8e04fb006
Timo <timowester@gmail.com>
2022-02-22 23:31:28 +0100

    Add posts specs

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /posts' do
+    before(:each) { get '/posts/:id/' }
+
+    it 'index body returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    
+    it 'index page includes correct placeholder text' do
+      expect(response.body).to include('this is the page for specific posts')
+    end
+  end
+
+  
+end
