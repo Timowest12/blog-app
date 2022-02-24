eecf9432d16ae1966baffb000662a60e3defebb8
Timo <timowester@gmail.com>
2022-02-24 01:22:44 +0100

    Add updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_231753) do
+ActiveRecord::Schema.define(version: 2022_02_24_002024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,12 +19,20 @@ ActiveRecord::Schema.define(version: 2022_02_23_231753) do
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
     t.integer "PostId"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -34,6 +42,8 @@ ActiveRecord::Schema.define(version: 2022_02_23_231753) do
     t.integer "likescounter"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "posts_id", null: false
+    t.index ["posts_id"], name: "index_posts_on_posts_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,4 +55,9 @@ ActiveRecord::Schema.define(version: 2022_02_23_231753) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "posts", column: "posts_id"
 end
