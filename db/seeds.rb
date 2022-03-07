aab75ccd6816e342dacbfe24942897d4ff44f1e7
Timo <timowester@gmail.com>
2022-03-07 18:27:13 +0100

    Add correct seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
-                         postscounter: 0)
-second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.',
-                          postscounter: 0)
-third_user = User.create(name: 'Adam', photo: 'https://genericphoto.com/user1', bio: 'Software developer from Taiwan',
-                         postscounter: 0)
+user1 = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+user2 = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+user3 = User.create(name: 'Anna', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+
+post1 = Post.create(user: User.first, title: 'Hello', text: 'This is my first post')
+post1 = Post.create(user: User.second, title: 'Hello', text: 'This is my second post')
+post1 = Post.create(user: User.third, title: 'Hello', text: 'This is my third post')
+post1 = Post.create(user: User.first, title: 'Hello all', text: 'This is my fifth post')
+
+comment1 = Comment.create(post: Post.first, user: User.first, text: 'Hi Tom!' )
+comment2 = Comment.create(post: Post.first, user: User.first, text: 'Hi Tom!' )
+comment3 = Comment.create(post: Post.second, user: User.second, text: 'Hi Marco!' )
+comment4 = Comment.create(post: Post.second, user: User.second, text: 'Hi Marco!' )
+comment5 = Comment.create(post: Post.third, user: User.third, text: 'Hi Mike!' )
+comment6 = Comment.create(post: Post.third, user: User.third, text: 'Hi Mike!' )  
