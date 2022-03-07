710eaab0811592ce67a51797ad4e0673a157b1f5
Timo <timowester@gmail.com>
2022-03-07 18:15:38 +0100

    Remove frozen string literals

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :user_id
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
