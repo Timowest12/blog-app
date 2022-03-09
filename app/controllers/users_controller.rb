7f58c63d57adf5dfa1a3a80f86c4c0f45d5fcda3
Timo <timowester@gmail.com>
2022-03-09 16:20:45 +0100

    Add user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @list_post = @user.three_recent_post
+    @posts_list = @user.three_recent_post
+    
   end
 end
