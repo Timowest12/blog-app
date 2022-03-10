c506cff5a389354f04a65bf663d5250de511bba1
Timo <timowester@gmail.com>
2022-03-10 14:44:47 +0100

    Add like route

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get '/users/:user_id/posts/:id/create_comment' => 'posts#show'
   get '/users/:user_id/posts/' => 'posts#index'
   get '/users/:user_id/posts/:id/new_comment' => 'posts#new_comment'
+  get '/users/:user_id/posts/:id/new_like' => 'posts#new_like'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
