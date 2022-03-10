e22ae00d5b8d27867a12d0891de8f44f03e9dec7
Timo <timowester@gmail.com>
2022-03-10 15:48:02 +0100

    Add posts controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts_list = @user.three_recent_post
   end
+
+  def new_like
+    @user = User.find(params[:user_id])
+  end
+
+  def create_post
+    @user = User.find(params[:id])
+    @postt = params[:post]
+    newpost = Post.create(user_id: params[:id], title: @postt[:title], text: @postt[:text])
+    @posts_list = @user.three_recent_post
+    render :show
+  end
+
 end
