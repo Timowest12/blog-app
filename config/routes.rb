6ab8795a16f1ec9b3ae655848f4f14920b5ca8a5
Timo <timowester@gmail.com>
2022-03-10 13:14:01 +0100

    Add comment route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get '/posts' => 'posts#index'
   get '/users/:id' => 'users#show'
   get '/users/:user_id/posts/:id' => 'posts#show'
+  post '/users/:user_id/posts/:id/create_comment' => 'posts#create_comment'
+  get '/users/:user_id/posts/:id/create_comment' => 'posts#show'
   get '/users/:user_id/posts/' => 'posts#index'
+  get '/users/:user_id/posts/:id/new_comment' => 'posts#new_comment'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
