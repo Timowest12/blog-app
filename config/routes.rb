10f554d601e45bd6870ee5fe5abd4fcb064d05e7
Timo <timowester@gmail.com>
2022-03-10 15:48:29 +0100

    Add posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
   get '/users' => 'users#index'
   get '/posts' => 'posts#index'
   get '/users/:id' => 'users#show'
+  get '/users/:id/new_post' => 'users#new_post' #new post page
+  post '/users/:id/create_post' => 'users#create_post' #create post function
   get '/users/:user_id/posts/:id' => 'posts#show'
   post '/users/:user_id/posts/:id/create_comment' => 'posts#create_comment'
   get '/users/:user_id/posts/:id/create_comment' => 'posts#show'
   get '/users/:user_id/posts/' => 'posts#index'
+  get '/users/:user_id/:user_id/posts/' => 'posts#index'
   get '/users/:user_id/posts/:id/new_comment' => 'posts#new_comment'
   get '/users/:user_id/posts/:id/new_like' => 'posts#new_like'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
