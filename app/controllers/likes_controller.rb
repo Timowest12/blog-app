176c167982328924964f94c2d131548c5718da4c
Timo <timowester@gmail.com>
2022-03-11 17:56:13 +0100

    Fix double like mistake

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
   def new_like
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
-    @newlike = Like.create(post: @post, user_id: 1)
+    @newlike = Like.new(post: @post, user_id: 1)
     if @newlike.save 
         redirect_to "/users/#{params[:user_id]}/posts", flash: { notice: 'Liked!' }
     else
