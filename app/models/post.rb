73371938f49159a95ad44632b8861a235f57bdfb
Timo <timowester@gmail.com>
2022-03-15 23:53:43 +0100

    conditional rendering new post button

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,8 +9,12 @@ class Post < ApplicationRecord
   end
 
   after_save :update_posts_counter
-
   def update_posts_counter
     author.increment!(:posts_counter)
+  end
+  after_destroy :update_posts_counter
+
+  def decrement_posts_counter
+    author.decrement!(:posts_counter)
   end
 end
