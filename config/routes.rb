91aae2476d14efa43323885510fa39ef20fd6d70
Timo <timowester@gmail.com>
2022-03-19 22:14:03 +0100

    Delete duplicate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  get '/' => 'users#index'
-  get '/users' => 'users#index'
-  get '/posts' => 'posts#index'
-  get '/users/:id' => 'users#show'
+  root 'users#index' 
+
   get '/posts/:id/new_post' => 'posts#new_post' #new post page
   post '/posts/:id/create_post' => 'posts#create_post' #create post function
-  get '/users/:user_id/posts/:id' => 'posts#show'
   post '/users/:user_id/posts/:id/create_comment' => 'comments#create_comment'
   get '/users/:user_id/posts/:id/create_comment' => 'posts#show'
-  get '/users/:user_id/posts/:id/delete' => 'posts#delete' #delete post
-  get '/users/:user_id/posts/' => 'posts#index'
-  get '/users/:user_id/:user_id/posts/' => 'posts#index'
-  get '/api/users/:user_id/:user_id/posts/' => 'posts#index' #API posts index
   get '/users/:user_id/posts/:id/new_comment' => 'comments#new_comment' #new comment page
   get '/users/:user_id/posts/:id/comment/:comment_id/delete' => 'comments#delete' #delete comment
   get '/users/:user_id/posts/:id/new_like' => 'likes#new_like' #new like
@@ -25,10 +18,12 @@ Rails.application.routes.draw do
       end
     end
   end
-
-  resources :users, only: [] do
-    member do
-      get 'apitoken'
+  
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show new create destroy] do
+      resources :comments, only: %i[new_comment create destroy]
+      resources :likes, only: %i[create]
     end
   end
 end
+
